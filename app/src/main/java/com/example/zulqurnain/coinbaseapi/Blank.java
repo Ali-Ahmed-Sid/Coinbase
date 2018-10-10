c098ed0b2bd173e54925fd27987367f3495c920c
mhd-zulqarnain <zulqarnain.redcode@outlook.com>
2018-10-10 13:43:41 +0500

    coinbase

--- app/src/main/java/com/example/zulqurnain/coinbaseapi/Blank.java
+++ app/src/main/java/com/example/zulqurnain/coinbaseapi/Blank.java
@@ -5,6 +5,7 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.coinbase.android.sdk.OAuth;
@@ -22,49 +23,29 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class Blank extends AppCompatActivity {
 
     private String token;
+    private TextView tv_Amount;
+    private TextView tv_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blank);
-        String code  = getIntent().getData().getQueryParameter("code");
-
-
+        String code = getIntent().getData().getQueryParameter("code");
+        tv_Amount = findViewById(R.id.tv_Amount);
+        tv_email = findViewById(R.id.tv_email);
 
         String url = "https://www.coinbase.com/oauth/token?grant_type=authorization_code&" +
-                "code="+code+"&client_id=37ccb7e87145ca56b06554b50a6f54b9f39d9c3cecc798038fd767d2e903f6fe" +
+                "code=" + code + "&client_id=37ccb7e87145ca56b06554b50a6f54b9f39d9c3cecc798038fd767d2e903f6fe" +
                 "&client_secret=bdd4f8fa882bd2a2e054567779d3ba08645b4b98c5bc255ae00f3e0888935587&" +
                 "redirect_uri=coinbase-android-example://coinbase-oauth";
 
 
         new RetrieveFeedTask().execute(url);
-//        try {
-//       //   token =   cb.getTokens("37ccb7e87145ca56b06554b50a6f54b9f39d9c3cecc798038fd767d2e903f6fe","bdd4f8fa882bd2a2e054567779d3ba08645b4b98c5bc255ae00f3e0888935587","7690cfa026636504598606f1e0124bbf1713f599368fb9235ba60c14f088bc84","http://www.redcodetechnologies.com").toString();
-//
-//        } catch (CoinbaseException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-//        try {
-//            String c = cb.getBalance().getAmount().toString();
-//            Toast.makeText(this,c,Toast.LENGTH_LONG).show();
-//
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (CoinbaseException e) {
-//            e.printStackTrace();
-//        }
-//        System.out.println(token);
-
-
-       // Toast.makeText(this,"asdasd",Toast.LENGTH_LONG).show();
     }
 
     class RetrieveFeedTask extends AsyncTask<String, Void, String> {
@@ -91,25 +72,21 @@ public class Blank extends AppCompatActivity {
                     }
                     bufferedReader.close();
                     return stringBuilder.toString();
-                }
-                finally{
+                } finally {
                     urlConnection.disconnect();
                 }
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 Log.e("ERROR", e.getMessage(), e);
                 return null;
             }
         }
 
         protected void onPostExecute(String response) {
-            if(response == null) {
+            if (response == null) {
                 response = "THERE WAS AN ERROR";
-            }
-            else
-            {
+            } else {
                 try {
-                    String  token  =new JSONObject(response).getString("access_token");
+                    String token = new JSONObject(response).getString("access_token");
                     new MyAsyn().execute(token);
 
                 } catch (JSONException e) {
@@ -121,8 +98,9 @@ public class Blank extends AppCompatActivity {
         }
     }
 
-    class MyAsyn extends AsyncTask<String,Void,String>{
-        String val="";
+    class MyAsyn extends AsyncTask<String, Void, String> {
+        String val = "";
+
         @Override
         protected String doInBackground(String... strings) {
 
@@ -133,7 +111,12 @@ public class Blank extends AppCompatActivity {
             try {
                 val = cb.getUser().getEmail();
                 System.out.println(val);
+                tv_Amount.setText(cb.getAccounts().getAccounts().get(0).getBalance().toString());
+
+                tv_email.setText(val);
+
             } catch (Exception e) {
+                tv_email.setText("Error "+e);
                 e.printStackTrace();
             }
 
